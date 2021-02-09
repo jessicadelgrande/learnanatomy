3576e3b3fe58f7ed51c47e678bbf3d17d4601b04
jessicadelgrande <jessica.delgrande@gmail.com>
2021-02-09 16:15:47 -0500

    content structure in place. added gsap.

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,10 +1,113 @@
+// smooth scroll (gsap)
+const html = document.documentElement;
+const body = document.body;
+const scroller = {
+	target: document.querySelector("#scroll-container"),
+	ease: 0.1, // scroll speed
+	endY: 0,
+	y: 0,
+	resizeRequest: 1,
+	scrollRequest: 0,
+};
 
+let requestId = null;
 
-
-App.init = () => {
-	App.events();
-} //end of init();
-
-$(function() {
-	App.init();
+gsap.set(scroller.target, {
+	rotation: 0.01,
+	force3d: true
 });
+
+function updateScroller() {
+
+	const resized = scroller.resizeRequest > 0;
+
+	if (resized) {
+		const height = scroller.target.clientHeight;
+		body.style.height = height + "px";
+		scroller.resizeRequest = 0;
+	}
+
+	const scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+
+	scroller.endY = scrollY;
+	scroller.y += (scrollY - scroller.y) * scroller.ease;
+
+	if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+		scroller.y = scrollY;
+		scroller.scrollRequest = 0;
+	}
+
+	gsap.set(scroller.target, {
+		y: -scroller.y
+	});
+
+	requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+}
+
+function onScroll() {
+	scroller.scrollRequest++;
+	if (!requestId) {
+		requestId = requestAnimationFrame(updateScroller);
+	}
+}
+
+function onResize() {
+	scroller.resizeRequest++;
+	if (!requestId) {
+		requestId = requestAnimationFrame(updateScroller);
+	}
+}
+
+
+// toggles class on menu
+// manages display of selected content
+const chooseContent = () => {
+	const menuContainer = document.querySelector(".toggleMenu");
+	menuContainer.addEventListener("click", (e) => {
+		e.preventDefault();
+		menuContainer.classList.toggle("opened");
+	});
+
+	const clickedItem = document.querySelector(".toggleMenu ul");
+	clickedItem.addEventListener("click", (e) => {
+		e.preventDefault();
+
+		const aboutContent = document.querySelector(".about");
+		const portfolioContent = document.querySelector(".portfolio");
+		const contactContent = document.querySelector(".contact");
+		const contentOptionArray = [aboutContent, portfolioContent, contactContent];
+		contentOptionArray.forEach((option) => {
+			option.classList.remove("open");
+		});
+
+		if (e.target.matches("#aboutItem")) {
+			aboutContent.classList.add("open");
+		} else if (e.target.matches("#portfolioItem")) {
+			portfolioContent.classList.add("open");
+		} else if (e.target.matches("#contactItem")) {
+			contactContent.classList.add("open");
+		} else {
+			console.log("there's a problem retrieving the selection");
+		}
+
+		// updates gsap window height when new content is loaded
+		updateScroller();
+	});
+}
+
+
+const onLoadHandler = () => {
+
+	chooseContent();
+	window.focus();
+	window.addEventListener("resize", onResize);
+	document.addEventListener("scroll", onScroll);
+
+}
+
+
+if (document.readyState === 'loading') {
+	document.addEventListener('DOMContentLoaded', onLoadHandler);
+} else {
+	onLoadHandler();
+}
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,111 @@
 "use strict";
 
-App.init = function () {
-	App.events();
-}; //end of init();
+// smooth scroll (gsap)
+var html = document.documentElement;
+var body = document.body;
+var scroller = {
+	target: document.querySelector("#scroll-container"),
+	ease: 0.1, // scroll speed
+	endY: 0,
+	y: 0,
+	resizeRequest: 1,
+	scrollRequest: 0
+};
 
-$(function () {
-	App.init();
+var requestId = null;
+
+gsap.set(scroller.target, {
+	rotation: 0.01,
+	force3d: true
 });
+
+function updateScroller() {
+
+	var resized = scroller.resizeRequest > 0;
+
+	if (resized) {
+		var height = scroller.target.clientHeight;
+		body.style.height = height + "px";
+		scroller.resizeRequest = 0;
+	}
+
+	var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+
+	scroller.endY = scrollY;
+	scroller.y += (scrollY - scroller.y) * scroller.ease;
+
+	if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+		scroller.y = scrollY;
+		scroller.scrollRequest = 0;
+	}
+
+	gsap.set(scroller.target, {
+		y: -scroller.y
+	});
+
+	requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+}
+
+function onScroll() {
+	scroller.scrollRequest++;
+	if (!requestId) {
+		requestId = requestAnimationFrame(updateScroller);
+	}
+}
+
+function onResize() {
+	scroller.resizeRequest++;
+	if (!requestId) {
+		requestId = requestAnimationFrame(updateScroller);
+	}
+}
+
+// toggles class on menu
+// manages display of selected content
+var chooseContent = function chooseContent() {
+	var menuContainer = document.querySelector(".toggleMenu");
+	menuContainer.addEventListener("click", function (e) {
+		e.preventDefault();
+		menuContainer.classList.toggle("opened");
+	});
+
+	var clickedItem = document.querySelector(".toggleMenu ul");
+	clickedItem.addEventListener("click", function (e) {
+		e.preventDefault();
+
+		var aboutContent = document.querySelector(".about");
+		var portfolioContent = document.querySelector(".portfolio");
+		var contactContent = document.querySelector(".contact");
+		var contentOptionArray = [aboutContent, portfolioContent, contactContent];
+		contentOptionArray.forEach(function (option) {
+			option.classList.remove("open");
+		});
+
+		if (e.target.matches("#aboutItem")) {
+			aboutContent.classList.add("open");
+		} else if (e.target.matches("#portfolioItem")) {
+			portfolioContent.classList.add("open");
+		} else if (e.target.matches("#contactItem")) {
+			contactContent.classList.add("open");
+		} else {
+			console.log("there's a problem retrieving the selection");
+		}
+
+		// updates gsap window height when new content is loaded
+		updateScroller();
+	});
+};
+
+var onLoadHandler = function onLoadHandler() {
+
+	chooseContent();
+	window.focus();
+	window.addEventListener("resize", onResize);
+	document.addEventListener("scroll", onScroll);
+};
+
+if (document.readyState === 'loading') {
+	document.addEventListener('DOMContentLoaded', onLoadHandler);
+} else {
+	onLoadHandler();
+}
